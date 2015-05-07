aaf48bee44221af9d25c2c952d7621111ff89c61
Tejas Ranade <tejas.ranade@gmail.com>
2015-05-06 22:48:39 -0400

    improve image loading

--- Flickr Fetchr/PhotoGridCell.m
+++ Flickr Fetchr/PhotoGridCell.m
@@ -13,19 +13,23 @@
 
 - (void) loadImage {
     if(self.photo){
+        self.photoView.image = nil;
         self.loadingSpinner.hidesWhenStopped = YES;
         [self.loadingSpinner startAnimating];
 
-        NSOperationQueue* imageLoadingQueue = [[NSOperationQueue alloc] init];
-        [imageLoadingQueue addOperationWithBlock:^{
-            NSData* data = [[NSData alloc] initWithContentsOfURL: [NSURL URLWithString: self.photo.media]];
-            if(!data) return;
-            
-            [[NSOperationQueue mainQueue] addOperationWithBlock:^{
+        dispatch_async(dispatch_get_global_queue(0,0), ^{
+            NSData * data = [[NSData alloc] initWithContentsOfURL: [NSURL URLWithString: self.photo.media]];
+            if ( data == nil )
+                return;
+            dispatch_async(dispatch_get_main_queue(), ^{
                 [self.loadingSpinner stopAnimating];
-                self.photoView.image = [UIImage imageWithData:data];
-            }];
-        }];
+                if(!self.photoView.image){
+                    self.photoView.image = [UIImage imageWithData:data];
+                }
+                
+            });
+        });
+        
     }
     //else load a default image
 }
